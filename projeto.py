031ecaba0e98ff95b1c5c645cb1c62e66dbf47d2
Eduardo Vianna Loewem <eduardoloewem@gmail.com>
2022-10-27 11:40:04 -0300

    Segunda versao do modelo

--- projeto.py
+++ projeto.py
@@ -5,7 +5,7 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
-# the data, split between train and test sets
+# dividindo os dados entre set de treinamento e set de teste
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 print(x_train.shape, y_train.shape)
@@ -14,7 +14,7 @@ x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
 
-# convert class vectors to binary class matrices
+# convertendo vetores em matrizes binárias para poder serem alimentados no modelo
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
@@ -30,6 +30,7 @@ batch_size = 128
 num_classes = 10
 epochs = 10
 
+# criando modelo de CNN, utilizando ativação do tipo relu e utilizando o Adadelta para otimização
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5),activation='relu',input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -44,9 +45,11 @@ model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
 
+#treinando o modelo, utilizando dados de treinamento, dados de validação, épocas e batch
 hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
 print("The model has successfully trained")
 
+#analisando a acurácia do modelo
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
